1b3bf910c92381bd7be66a8091a1725322b7b9a4
krishna-002 <94431698+krishna-002@users.noreply.github.com>
2024-10-27 16:31:43 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============= typing animation ================= */
+var typed = new Typed(".typing",{
+    strings:["","Philomath","Aspiring Software Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
